81b01e69c65545301c0501683f26730155ec1463
jbranchaud <jbranchaud@gmail.com>
2013-05-31 22:36:50 -0500

    Adding some alphabet magic to the repo --- [alphabet.yml] Adding a set of alphabet letters that can read in as data for creating pixel art. [alphabet.rb] A few methods for reading YAML and outputting the data in different ways. [alphabet.rb=alphabet.yml] alphabet.rb reads the alphabet.yml file in order to get that letter data that it outputs. The format that it expects is specific to the alphabet.yml file. ...

---
+++ alphabet.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+# alphabet.rb
+#
+# this module grabs alphabet letters from the alphabet.yml file and allows
+# you to print them out or builds and returns a hash map of their data.
+
+require 'yaml'
+
+module Alphabet
+
+  YAML_FILE = File.dirname(__FILE__) + '/alphabet.yml'
+  DOCUMENT = YAML.load_file(YAML_FILE)
+
+  def self.print_letter(letter)
+    curr = DOCUMENT[letter]
+    width = curr[0]['width']
+    height = curr[1]['height']
+    chars = curr[2]['chars']
+    (1..height).each do |n|
+      puts chars[(n-1)*width,width]
+    end
+    puts chars
+  end
+
+  def self.get_letter_chars(letter)
+    curr = DOCUMENT[letter]
+    width = curr[0]['width']
+    height = curr[1]['height']
+    chars = curr[2]['chars']
+    return chars
+  end
+
+  def self.letter_to_hash(letter)
+    curr = DOCUMENT[letter]
+    width = curr[0]['width']
+    height = curr[1]['height']
+    chars = curr[2]['chars']
+    char_map = Hash.new
+    (1..width).each do |n|
+      (1..height).each do |m|
+        pos = (n-1) + (m-1)*width
+        char = chars[pos]
+        char_map[[n-1,m-1]] = char
+      end
+    end
+    return char_map
+  end
+
+end
