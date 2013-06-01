ffc4150962f5d052e701e186172caa7ae0294423
jbranchaud <jbranchaud@gmail.com>
2013-06-01 15:30:45 -0500

    Adding the new commitart.rb file --- [commitart.rb] This file has all the pieces for creating the contribution graph commit art, it does still need to be tested though. ...

---
+++ commitart.rb
@@ -0,0 +1,106 @@
+#!/usr/bin/env ruby
+
+# commitart.rb
+#
+# this script will utilize the rugged rubygem to create a git history that
+# spells things in the contribution graph on GitHub.
+
+require 'rugged'
+
+require File.dirname(__FILE__) + '/alphabet'
+
+module CommitArt
+
+  # MULTIPLIER - this value represents the number to multiply the letter map
+  # value by which results in the number of commits to be created for that
+  # slot.
+  MULTIPLIER = 10
+
+  # create_commit_art
+  #
+  # given the full path of a git repository and a string of letters, this
+  # function is going to access the repository and create a series of
+  # commits for past dates in such a way that the resulting GitHub
+  # contribution graph spells out the given string of letters.
+  # Assuming the string of letters is no more than 7 letters.
+  # Assuming the given repository is not important as this script will
+  # clobber that repositories commit history.
+  def self.create_commit_art(repo_path, letters)
+    repo = Rugged::Repository.new(repo_path)
+    # make sure we only working with no more than 7 letters
+    chars = letters[0,7]
+    char_maps = chars.split("").map do |char|
+      Alphabet.letter_to_hash(char)
+    end
+    char_strings = chars.split("").map do |char|
+      Alphabet.get_letter_chars(char)
+    end
+    ref_date = find_oldest_sunday
+    char_strings.each do |char_string|
+      char_string.split("").each do |char|
+        create_commits(repo,ref_date,char.to_i*MULTIPLIER)
+        ref_date = add_day(ref_date)
+      end
+    end
+  end
+
+  # find_oldest_sunday
+  #
+  # based on the current date, this function will find the sunday closest to
+  # 365 days ago (but on the lower bound of it). For instance, if there is a
+  # Sunday 362 days ago, then it is that Time object that is returned.
+  def self.find_oldest_sunday
+    one_year = 365*24*60*60
+    one_day = 24*60*60
+    curr_day = Time.now.to_i - one_year
+    year_ago = Time.at(curr_day)
+    sunday = false
+    while !Time.at(curr_day).sunday?
+      curr_day += one_day
+    end
+    return Time.at(curr_day)
+  end
+
+  # add_day
+  #
+  # given a Time object, this function will add a day to the date and return
+  # the new Time object.
+  def self.add_day(date)
+    one_day = 24*60*60
+    next_day = date.to_i + one_day
+    return Time.at(next_day)
+  end
+
+  def self.get_repo(repo_path)
+    return Rugged::Repository.new(repo_path)
+  end
+
+  EMAIL = "commitart1337@gmail.com"
+  USERNAME = "commitart"
+
+  def self.create_commits(repo, date, count)
+    # if the count is 0, then get out of here
+    if count <= 0
+      return
+    end
+    # get the stuff we need from the repo (head commit, tree, etc.)
+    curr_ref = "HEAD"
+    curr_commit = repo.lookup(repo.head.target)
+    curr_tree = curr_commit.tree
+    # create the author hash
+    time = date.to_i
+    count.times do |x|
+      author = {:email=>EMAIL,:time=>Time.at(time),:name=>USERNAME}
+      curr_commit = Rugged::Commit.create(repo,
+                            :author => author,
+                            :message => "CommitArt commit at #{Time.at(time)}.",
+                            :committer => author,
+                            :parents => ["#{curr_commit.oid}"],
+                            :tree => curr_tree,
+                            :update_ref => curr_ref)
+      curr_tree = curr_commit.tree
+      time = time + 1
+    end
+  end
+
+end
